f0536c57375d194caa319e3f94c7801343b94843
eylemugurel <eylemugurel@hotmail.com>
2025-01-23 21:39:11 +0100

    Add essential files

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+/**
+ * index.php
+ *
+ * (C) 2025 by Eylem Ugurel
+ *
+ * Licensed under a Creative Commons Attribution 4.0 International License.
+ *
+ * You should have received a copy of the license along with this work. If not,
+ * see <http://creativecommons.org/licenses/by/4.0/>.
+ */
+
+header('Location: source/');
