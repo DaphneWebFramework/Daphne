756da8b97ed380971d97df28527c7ed71a5c7472
eylemugurel <eylemugurel@hotmail.com>
2025-01-25 10:30:40 +0100

    Load configuration options file

--- source/bootstrap.php
+++ source/bootstrap.php
@@ -2,13 +2,16 @@
 /**
  * bootstrap.php
  *
- * (C) 2024 by Eylem Ugurel
+ * (C) 2025 by Eylem Ugurel
  *
  * Licensed under a Creative Commons Attribution 4.0 International License.
  *
  * You should have received a copy of the license along with this work. If not,
  * see <http://creativecommons.org/licenses/by/4.0/>.
  */
+
+use \Harmonia\Config;
+use \Harmonia\Core\CPath;
 
 \spl_autoload_register(function(string $className): void {
     $classFilePath = \rtrim(__DIR__, '/\\')
@@ -20,3 +23,5 @@
     }
     require $classFilePath;
 });
+
+Config::Instance()->Load(CPath::Join(__DIR__, 'config.inc.php'));
