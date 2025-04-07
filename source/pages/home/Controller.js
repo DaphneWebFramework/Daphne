013b4f809fa7319ef84c2b41a24671dc33a39e48
eylemugurel <eylemugurel@hotmail.com>
2025-04-07 23:41:44 +0200

    Integrate Leuce MVC classes into home page structure

---
+++ source/pages/home/Controller.js
@@ -0,0 +1,17 @@
+/**
+ * Controller.js
+ *
+ * (C) 2025 by Eylem Ugurel
+ *
+ * Licensed under a Creative Commons Attribution 4.0 International License.
+ *
+ * You should have received a copy of the license along with this work. If not,
+ * see <http://creativecommons.org/licenses/by/4.0/>.
+ */
+
+class Controller extends Leuce.MvcController
+{
+  constructor(model, view) {
+    super(model, view);
+  }
+}
