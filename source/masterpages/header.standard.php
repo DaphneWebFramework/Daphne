8e264eef125a44b03d6225de68da604b743016c3
eylemugurel <eylemugurel@hotmail.com>
2025-04-27 04:53:00 +0200

    Add "standard" masterpage

---
+++ source/masterpages/header.standard.php
@@ -0,0 +1,89 @@
+<?php declare(strict_types=1);
+/**
+ * header.standard.php
+ *
+ * (C) 2025 by Eylem Ugurel
+ *
+ * Licensed under a Creative Commons Attribution 4.0 International License.
+ *
+ * You should have received a copy of the license along with this work. If not,
+ * see <http://creativecommons.org/licenses/by/4.0/>.
+ */
+
+if (!isset($this) || !$this instanceof \Peneus\Systems\PageSystem\Page) {
+	exit;
+}
+
+use \Harmonia\Config;
+use \Peneus\Resource;
+use \Peneus\Model\Role;
+use \Charis\{
+	Container,
+	Navbar,
+	NavbarBrand,
+	NavbarToggler,
+	NavbarCollapse,
+	NavbarNav,
+	NavbarItem,
+	NavbarDropdown,
+	NavbarDropdownItem,
+	NavbarDropdownDivider
+};
+
+$config = Config::Instance();
+$resource = Resource::Instance();
+$account = $this->LoggedInAccount();
+$role = $this->LoggedInAccountRole();
+$navItems = [];
+
+if ($account === null) {
+	$navItems[] = new NavbarItem([
+		':label' => 'Register',
+		':href' => $resource->PageUrl('register')
+	]);
+	$navItems[] = new NavbarItem([
+		':label' => 'Login',
+		':href' => $resource->LoginPageUrl()
+	]);
+} else {
+	$dropdownItems = [
+		new NavbarDropdownItem([
+			':label' => 'Settings',
+			':href' => $resource->PageUrl('settings')
+		])
+	];
+	if ($role >= Role::Editor) {
+		$dropdownItems[] = new NavbarDropdownDivider();
+		$dropdownItems[] = new NavbarDropdownItem([
+			':label' => 'Editor',
+			':href' => $resource->PageUrl('editor')
+		]);
+	}
+	if ($role >= Role::Admin) {
+		$dropdownItems[] = new NavbarDropdownItem([
+			':label' => 'Admin',
+			':href' => $resource->PageUrl('admin')
+		]);
+	}
+	$dropdownItems[] = new NavbarDropdownDivider();
+	$dropdownItems[] = new NavbarDropdownItem([
+		':label' => 'Log out',
+		':id' => 'logout'
+	]);
+	$navItems[] = new NavbarDropdown([
+		':label' => $account->displayName
+	], $dropdownItems);
+}
+?>
+	<?=new Navbar(['class' => 'bg-dark navbar-expand-sm', 'data-bs-theme' => 'dark'], [
+		new Container(null, [
+			new NavbarBrand(['href' => $resource->AppUrl()], $config->Option('AppName')),
+			new NavbarToggler([
+				'data-bs-target' => '#navbarTogglerTarget',
+				'aria-controls' => 'navbarTogglerTarget'
+			]),
+			new NavbarCollapse(['id' => 'navbarTogglerTarget'], [
+				new NavbarNav(['class' => 'ms-auto'], $navItems)
+			])
+		])
+	])?>
---
+++ source/masterpages/standard.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1);
+/**
+ * standard.php
+ *
+ * (C) 2025 by Eylem Ugurel
+ *
+ * Licensed under a Creative Commons Attribution 4.0 International License.
+ *
+ * You should have received a copy of the license along with this work. If not,
+ * see <http://creativecommons.org/licenses/by/4.0/>.
+ */
+
+if (!isset($this) || !$this instanceof \Peneus\Systems\PageSystem\Page) {
+	exit;
+}
+?>
+	<!-- Header -->
+<?php include 'header.standard.php'?>
+	<!-- End Header -->
+
+	<!-- Content -->
+<?=$this->Content()?>
+	<!-- End Content -->
+
+	<!-- Footer -->
+<?php include 'footer.php'?>
+	<!-- End Footer -->
