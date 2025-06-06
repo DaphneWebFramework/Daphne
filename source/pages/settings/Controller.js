03828de65ee7b71bc749c3e9f3a85b13f7f77264
eylemugurel <eylemugurel@hotmail.com>
2025-06-06 11:49:28 +0200

    Settings page: Initial structure and Account pane layout

---
+++ source/pages/settings/Controller.js
@@ -0,0 +1,22 @@
+/**
+ * Controller.js
+ *
+ * (C) 2025 by Eylem Ugurel
+ *
+ * Licensed under a Creative Commons Attribution 4.0 International License.
+ *
+ * You should have received a copy of the license along with this work. If not,
+ * see <http://creativecommons.org/licenses/by/4.0/>.
+ */
+
+class Controller extends App.Controller
+{
+    /**
+     * @param {Model} model
+     * @param {View} view
+     */
+    constructor(model, view)
+    {
+        super(model, view);
+    }
+}
---
+++ source/pages/settings/index.php
@@ -0,0 +1,126 @@
+<?php declare(strict_types=1);
+/**
+ * index.php
+ *
+ * (C) 2025 by Eylem Ugurel
+ *
+ * Licensed under a Creative Commons Attribution 4.0 International License.
+ *
+ * You should have received a copy of the license along with this work. If not,
+ * see <http://creativecommons.org/licenses/by/4.0/>.
+ */
+
+require '../../autoload.php';
+
+use \Charis\Button;
+use \Charis\Form;
+use \Charis\FormComposites\FormCheck;
+use \Charis\FormComposites\FormPassword;
+use \Charis\FormComposites\FormText;
+use \Charis\FormControls\FormEmailInput;
+use \Charis\Generic;
+use \Charis\PillTab;
+use \Charis\TabPane;
+use \Charis\TabPanes;
+use \Charis\VerticalPillTabNavigation;
+use \Charis\VerticalPillTabs;
+use \Peneus\Resource;
+use \Peneus\Systems\PageSystem\Page;
+
+$page = (new Page(__DIR__))
+	->SetTitle('Settings')
+	->SetMasterPage('standard')
+	->RequireLogin()
+	->AddLibrary('bootstrap-icons')
+	->SetProperty('wideLayout', true);
+
+$resource = Resource::Instance();
+?>
+<?php $page->Begin()?>
+	<main role="main">
+		<?=new VerticalPillTabNavigation(['class' => '-align-items-start'], [
+			new VerticalPillTabs(['class' => '-me-3 bg-light'], [
+				new PillTab([':key' => 'account', ':active' => true], [
+					new Generic('i', ['class' => 'bi bi-person-circle']),
+					new Generic('span', ['class' => 'label'], 'Account')
+				]),
+				new PillTab([':key' => 'preferences'], [
+					new Generic('i', ['class' => 'bi bi-sliders']),
+					new Generic('span', ['class' => 'label'], 'Preferences')
+				]),
+			]),
+			new TabPanes([], [
+				new TabPane([':key' => 'account', ':active' => true], [
+					new Generic('h3', null, 'Account'),
+					new Generic('section', null, [
+						new Form(null, [
+							new Generic('div', ['class' => 'd-flex align-items-end gap-2 mb-3'], [
+								new FormText([
+									':label' => 'Display name',
+									':name' => 'displayName',
+									':required' => true,
+									'class' => '-mb-3 flex-grow-1'
+								]),
+								new Button([
+									'type' => 'submit',
+									'class' => 'btn-outline-secondary'
+								], 'Save')
+							])
+						])
+					]),
+					new Generic('section', null, [
+						new Generic('h5', null, 'Change password'),
+						new Form(null, [
+							new FormEmailInput([
+								'class' => 'd-none',
+								'value' => '',
+								'autocomplete' => 'username'
+							]),
+							new FormPassword([
+								':label' => 'Current password',
+								':name' => 'currentPassword',
+								':autocomplete' => 'off',
+								':required' => true
+							]),
+							new FormPassword([
+								':label' => 'New password',
+								':name' => 'newPassword',
+								':autocomplete' => 'new-password',
+								':required' => true
+							]),
+							new Generic('div', ['class' => 'd-flex justify-content-between align-items-center'], [
+								new Generic('a', [
+									'href' => $resource->PageUrl('forgot-password')
+								], 'Forgot your password?'),
+								new Button([
+									'type' => 'submit',
+									'class' => 'btn-outline-secondary'
+								], 'Save')
+							])
+						])
+					]),
+					new Generic('section', null, [
+						new Generic('h5', null, 'Delete account'),
+						new Generic('p', null, 'Deleting your account will permanently erase all your data. This action cannot be undone.'),
+						new Form(null, [
+							new FormCheck([
+								':label' => 'I understand this deletion is permanent.',
+								':required' => true,
+								'class' => 'mb-3'
+							]),
+							new Button([
+								'type' => 'submit',
+								'class' => 'btn-outline-danger',
+								'disabled' => true
+							], 'Delete Account')
+						])
+					])
+				]),
+				new TabPane([':key' => 'preferences'], [
+					new Generic('h3', null, 'Preferences'),
+					new Generic('p', null, 'Set your language, theme, and notification preferences.')
+				])
+			])
+		])?>
+	</main>
+<?php $page->End()?>
